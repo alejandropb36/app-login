aee28350d6520e4a92532dd7602f9fa409720d2c
Alejandro Ponce <alexponce36@hotmail.com>
2023-09-12 16:07:39 -0600

    chore: remove material

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  user: User | undefined;
+
+  constructor(private readonly loginService: LoginService){}
+
+  ngOnInit(): void {
+    this.loginService.getProfile().subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (err) => {
+        debugger;
+        console.error(err);
+
+        
+      }
+    })
+  }
+
 
 }
