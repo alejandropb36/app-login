bbc63dd72ef00d80d6a0c0a6aa29fc259a145546
Alejandro Ponce <alexponce36@hotmail.com>
2023-09-12 20:23:08 -0600

    feat: login and redirect to profile

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginUser } from 'src/app/models/login.model';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +11,31 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(
+    private readonly loginService: LoginService,
+    private readonly router: Router) {}
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  onSubmit(): void {
+    console.log({value: this.loginForm.value, valid: this.loginForm.valid});
+
+    const userLogin = this.loginForm.value as LoginUser;
+
+    this.loginService.login(userLogin).subscribe({
+      next: (response) => {
+        this.router.navigate(['/profile']);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }
+
+  goToRegister(): void {
+    this.router.navigate(['/register']);
+  }
 }
